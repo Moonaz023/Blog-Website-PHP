eb1c25307619d303c2ba58dcf71e8432bed4e363
Md. Moonaz Rahman <60685959+Moonaz023@users.noreply.github.com>
2022-10-31 02:14:43 +0600

    Add files via upload
    web Engineering assignment

---
+++ 404.php
@@ -0,0 +1,11 @@
+<?PHP include 'inc/header.php';?>
+	<div class="contentsection contemplete clear">
+		<div class="maincontent clear">
+			<div class="about">
+				<div class="notfound">
+    				<p><span>404</span> Not Found</p>
+    			</div>
+	        </div>
+</div>
+<?PHP include "inc/sidebar.php";?>
+<?PHP include 'inc/footer.php';?>
