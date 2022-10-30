4e14e00aee32f25ccd280b07d0a27084e55f13f4
Md. Moonaz Rahman <60685959+Moonaz023@users.noreply.github.com>
2022-10-31 02:21:32 +0600

    Add files via upload

---
+++ admin/inc/footer.php
@@ -0,0 +1,12 @@
+<div class="clear">
+        </div>
+    </div>
+    <div class="clear">
+    </div>
+    <div id="site_info">
+        <p>
+         &copy; Copyright <a href="profile.html" target="_blank">Md. Moonaz Rahman</a>. All Rights Reserved.
+        </p>
+    </div>
+</body>
+</html>
